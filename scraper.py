66f8ae3f1eb3041f6e4b882e50126c145e5a2d10
Tanuj Kumar <63194483+TanujKumar-codeX@users.noreply.github.com>
2021-12-14 13:57:41 +0000

    made some helper functions(get_driver and get_videos) and found no of trending videos

--- scraper.py
+++ scraper.py
@@ -1,8 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 Youtube_trending_url = 'https://www.youtube.com/feed/trending'
 
-driver = webdriver.Chrome()
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
 
-driver.get(Youtube_trending_url)
-print(driver.title)
+def get_videos(driver):
+  driver.get(Youtube_trending_url)
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  videos = driver.find_elements(By.TAG_NAME,VIDEO_DIV_TAG)
+  return videos
+
+if __name__ == '__main__':
+  print('Getting Driver')
+  driver = get_driver()
+
+  print('Fetching Trending Videos')
+
+  video_divs = get_videos(driver)
+  print(f'Found {len(video_divs)} trending videos.')
+
