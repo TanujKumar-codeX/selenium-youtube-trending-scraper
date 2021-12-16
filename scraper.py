732d41f59174ac66061adb8a003f175226eac9c1
Tanuj Kumar <63194483+TanujKumar-codeX@users.noreply.github.com>
2021-12-16 05:46:14 +0000

    made send mail function and able to send json files.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import os
+import json
 import smtplib
 import pandas as pd
 from selenium import webdriver
@@ -42,16 +44,16 @@ def parse_video(video):
     'description':description_tag
   }
 
-def send_mail():
+def send_mail(body):
   try:
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server_ssl.ehlo()   # optional
     
+    SENDER_EMAIL = 'trensetterforyou@gmail.com'
+    RECIEVER_EMAIL = ['iitbtanuj@gmail.com','194113@nith.ac.in']
+    SENDER_PASSWORD = os.environ['GMAIL_PASSWORD']
+  
     subject = 'Test Message from REPLit'
-    body = 'Hey, this a test mail from REPLit'
-
-    SENDER_EMAIL = 'trendsetterforyou@gmail.com'
-    RECIEVER_EMAIL = 'iitbtanuj@gmail.com'
 
     email_text = f'''\
     From: {SENDER_EMAIL}
@@ -60,29 +62,37 @@ def send_mail():
 
     {body}
     '''
+    print(email_text)
+    server_ssl.login(SENDER_EMAIL,SENDER_PASSWORD)
+    server_ssl.sendmail(SENDER_EMAIL, RECIEVER_EMAIL, email_text)
+    server_ssl.close() 
+    print('Mail Sent')
   except:
       print('Something went wrong...')
 
 if __name__ == '__main__':
-  # print('Getting Driver')
-  # driver = get_driver()
+  print('Getting Driver')
+  driver = get_driver()
 
-  # print('Fetching Trending Videos')
+  print('Fetching Trending Videos')
 
-  # videos = get_videos(driver)
-  # print(f'Found {len(videos)} trending videos.')
+  videos = get_videos(driver)
+  print(f'Found {len(videos)} trending videos.')
 
-  # # title, url, channel, views, time uploaded, dicription, thumbnail url
+  # title, url, channel, views, time uploaded, dicription, thumbnail url
 
-  # print('Parsing Top 10 Videos.')
-  # videos_data = [parse_video(video) for video in videos[:10]]
+  print('Parsing Top 10 Videos.')
+  videos_data = [parse_video(video) for video in videos[:10]]
   
-  # print('Saving The Data To A CSV File.')
+  print('Saving The Data To A CSV File.')
 
-  # videos_df = pd.DataFrame(videos_data)
-  # print(videos_df)
+  videos_df = pd.DataFrame(videos_data)
+  #print(videos_df)
 
-  # videos_df.to_csv('Trending.csv',index=None)
+  videos_df.to_csv('Trending.csv',index=None)
 
-  print('send mail')
-  send_mail()
+  print('send the result over email.')
+  body = json.dumps(videos_data,indent=2)
+  send_mail(body)
+
+  print('Finished.')
